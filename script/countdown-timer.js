753d0a6ba921abd471960c4bde6c58372b7e7b5e
Rafael Chicarelli Lopes <rafaelsep20@gmail.com>
2022-11-14 21:47:57 -0300

    feat: init calc countdown

--- script/countdown-timer.js
+++ script/countdown-timer.js
@@ -1,6 +1,11 @@
 let elements;
+let timer;
 const LOCALSTORAGE_KEY = "timer";
 const YEAR = new Date().getFullYear();
+const DAYS_MS = 86400000;
+const HOURS_MS = 3600000;
+const MINUTES_MS = 60000;
+const SECONDS_MS = 1000;
 
 document.addEventListener("DOMContentLoaded", function () {
   elements = {
@@ -34,7 +39,10 @@ function preloadData() {
   elements.inputTimer.value = preConfiguredTime;
 }
 
-function startCountdown() {}
+function startCountdown() {
+  //TODO: Alteração de estilos para aparecer a hora.
+  updateCountdown();
+}
 
 function stopCountdown() {
   removeTime();
@@ -50,4 +58,32 @@ function setTime(time) {
 
 function removeTime() {
   window.localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const target = new Date(getTime()).getTime();
+  console.log({ target });
+  const difference = target - now;
+
+  if (difference <= 0) {
+    clearInterval(timer);
+    return;
+  }
+
+  const days = difference / DAYS_MILLISECONDS;
+  const hours = (difference % DAYS_MILLISECONDS) / HOURS_MILLISECONDS;
+  const minutes =
+    ((difference % DAYS_MILLISECONDS) % HOURS_MILLISECONDS) /
+    MINUTES_MILLISECONDS;
+  const seconds =
+    (((difference % DAYS_MILLISECONDS) % HOURS_MILLISECONDS) %
+      MINUTES_MILLISECONDS) /
+    SECONDS_MS;
+
+  console.log({
+    now,
+    target,
+    difference: target - now,
+  });
 }
